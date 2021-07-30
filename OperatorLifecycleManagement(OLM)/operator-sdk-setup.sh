0c9c49573c0f0525f849a298c8b4af35ea3ebff0
skmaifujalam <Newman123@>
2021-07-30 18:12:08 +0530

    Single master cluster

---
+++ OperatorLifecycleManagement(OLM)/operator-sdk-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
+export OS=$(uname | awk '{print tolower($0)}')
+echo $ARCH
+echo $OS
