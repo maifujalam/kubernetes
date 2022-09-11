35b0a724eef032b80fc0d97e5a9d66edc4b2b9a6
maifujalam <sk.maifujalam@gmail.com>
2022-09-11 19:50:36 +0530

    k8s-1.25_base-v1.0

---
+++ vm-provision/k8s_localhost/ShellScripts/k8s_master_bootstrap.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+FILE=/home/vagrant/.kube/config
+if [ -f "$FILE" ]; then
+    printf "\n Deleting existing k8s Cluster...\n\n"
+    kubeadm reset --force
+fi
+printf "\nInitializing Cluster...\n\n"
+  kubeadm init --config /tools/kubeadm-init/init-default.yaml
+
+printf "\nCopying Config Files...\n\n"
+  su - vagrant -c 'mkdir -p $HOME/.kube'
+  su - vagrant -c 'sudo cp -vf /etc/kubernetes/admin.conf $HOME/.kube/config'
+  su - vagrant -c 'sudo cp -vf /etc/kubernetes/admin.conf /vagrant/config'
+  su - vagrant -c 'sudo chown $(id -u):$(id -g) $HOME/.kube/config'
+
+printf "\nsleeping for 5 seconds...\n"
+sleep 5
+
+printf "\nInstalling Tigera Operator for Canico CNI...\n\n"
+  su - vagrant -c 'kubectl create -f /tools/manifests/tigera-operator-v3.24.1.yaml'
+
+printf "\nInstalling Calico CNI with VXLAN...\n\n"
+  su - vagrant -c 'kubectl create -f /tools/manifests/calico-install-vxlan.yaml'
+
+printf "\nInstalling metric server...\n\n"
+   su - vagrant -c 'kubectl apply -f /tools/manifests/metric-server-v0.6.1.yaml'
+
+printf "\nInstalling k8s Dashboard...\n\n"
+  su - vagrant -c 'kubectl create -f /tools/manifests/k8s-dashboard-v2.6.1.yaml'
+
+printf "\nCooling down for 1 minute...\n"
+  sleep 60
+
+printf "\n Creating dashboard User...\n\n"
+  su - vagrant -c 'kubectl apply -f /tools/manifests/dahboard-admin-user.yaml'
+
+printf "\n Extracting dashboard token\n"
+  su - vagrant -c 'kubectl -n kubernetes-dashboard create token admin-user >> /vagrant/dashboard_token.txt'
