dd34a5f3c2856fa06a0f096ae365225c0bed1bc1
alam <sk.maifujalam@gmail.com>
2023-08-12 10:37:59 +0530

    Packages and push

--- vm-provision/k8s_localhost/ShellScripts/install_crictl.sh
+++ vm-provision/k8s_localhost/ShellScripts/install_crictl.sh
@@ -2,3 +2,4 @@ VERSION="v1.27.1"
 wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
 sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
 rm -f crictl-$VERSION-linux-amd64.tar.gz
+crictl version
