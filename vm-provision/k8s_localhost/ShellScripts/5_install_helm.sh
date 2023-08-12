1fd44a6217e90a9156473feeeed81e3aabe7e2b2
alam <sk.maifujalam@gmail.com>
2023-08-12 11:40:34 +0530

    k8s-virtualbox1.0-beta

--- vm-provision/k8s_localhost/ShellScripts/5_install_helm.sh
+++ vm-provision/k8s_localhost/ShellScripts/5_install_helm.sh
@@ -6,5 +6,5 @@ if [[ ! -f helm-$VERSION-linux-amd64.tar.gz ]];then
 fi
 sudo tar -zxvf helm-$VERSION-linux-amd64.tar.gz
 sudo cp linux-amd64/helm /usr/local/bin/
-rm -frv helm-linux-amd64
+rm -frv linux-amd64
 helm version
