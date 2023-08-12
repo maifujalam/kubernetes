e94bbc4c8c436eb0771be88dd250c04006b6f909
alam <sk.maifujalam@gmail.com>
2023-08-12 10:41:24 +0530

    k8s-virtualbox1.0-beta

--- vm-provision/k8s_localhost/ShellScripts/download-packages.sh
+++ vm-provision/k8s_localhost/ShellScripts/download-packages.sh
@@ -10,5 +10,20 @@ gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 sudo yum clean all
 #sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y containerd kubelet kubeadm kubectl --downloadonly --downloaddir=/vagrant/packages/.
 sudo rpm -Uhv /vagrant/packages/*.rpm
+
+###### Verify Containerd  ########
+containerd -v
+sudo systemctl enable --now containerd.service
+
+########### Kubelet #############
+kubelet --version
+sudo systemctl enable --now kubelet
+
+############ kubeadm ######
+kubeadm version
+
+########## kubectl #######
+kubectl version
