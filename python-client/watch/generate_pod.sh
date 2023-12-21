8140cbe83cdd47c1fb76aacb0336d58c9bf8cfa5
alam <sk.maifujalam@gmail.com>
2023-12-21 20:09:54 +0530

    k8s_v1.27.4_base first

--- python-client/watch/generate_pod.sh
+++ python-client/watch/generate_pod.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
-kubectl run busybox0 --image=busybox --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
+kubectl run busybox0 --image=busybox --image-pull-policy=IfNotPresent --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
 
 #!/bin/bash
-kubectl run busybox1 --image=busybox --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
+kubectl run busybox1 --image=busybox --image-pull-policy=IfNotPresent --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
 echo "sleeping for 30s"
-sleep 60
+sleep 10
 
-kubectl run busybox2 --image=busybox --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
+kubectl run busybox2 --image=busybox --image-pull-policy=IfNotPresent --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
 echo "sleeping for 30s"
-sleep 60
+sleep 10
 
-kubectl run busybox3 --image=busybox --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
+kubectl run busybox3 --image=busybox --image-pull-policy=IfNotPresent --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
 echo "sleeping for 30s"
-sleep 60
+sleep 10
 
-kubectl run busybox4 --image=busybox --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
+kubectl run busybox4 --image=busybox --image-pull-policy=IfNotPresent --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
 echo "sleeping for 30s"
-sleep 60
+sleep 10
 
-kubectl run busybox5 --image=busybox --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
+kubectl run busybox5 --image=busybox --image-pull-policy=IfNotPresent --restart=Never -- /bin/sh -c "sleep 3600;echo boo"
 echo "sleeping for 30s"
-sleep 60
+sleep 10
 
 kubectl delete po busybox1
 echo "sleeping for 30s"
-sleep 60
+sleep 10
 
 kubectl delete po busybox2
 echo "sleeping for 30s"
-sleep 60
+sleep 10
 
 kubectl delete po busybox3
 echo "sleeping for 30s"
-sleep 60
+sleep 10
 
 kubectl delete po busybox4
 echo "sleeping for 30s"
-sleep 60
+sleep 10
 
 kubectl delete po busybox5
 echo "sleeping for 30s"
